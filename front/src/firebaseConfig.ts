022e36977a7d4199de4443a90ae01d2bb424d372
sun <sunyp0713@naver.com>
2026-02-06 03:20:30 +0900

    파이어베이스 설정

---
+++ front/src/firebaseConfig.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIRE_API_KEY,
+  authDomain: import.meta.env.VITE_FIRE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIRE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIRE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIRE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIRE_APP_ID,
+  measurementId: import.meta.env.VITE_FIRE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
